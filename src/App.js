5492145d50be25bd6f9f2cab6b82ed6b4a217de8
prama pandit <panditprama@gmail.com>
2022-04-20 15:36:26 +0545

    Create Showfile file and set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Calc from './Component/Calculater/Calc'
 import Main from './Component/ExportImport/Main'
+import Showfile from './Component/ShowHide/Showfile'
 
 const App = () => {
   return (
     <>
       {/* <Calc /> */}
-      <Main />
+      {/* <Main /> */}
+      <Showfile/>
     </>
   )
 }
