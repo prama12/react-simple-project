324d32953d01967bc451758a4bd2c8b61e211df3
prama pandit <panditprama@gmail.com>
2022-04-20 13:48:02 +0545

    Create exportimport folder in component and also create main file and set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Calc from './Component/Calculater/Calc'
+import Main from './Component/ExportImport/Main'
 
 const App = () => {
   return (
     <>
-      <Calc />
+      {/* <Calc /> */}
+      <Main />
     </>
   )
 }
