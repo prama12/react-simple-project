8dcf192026a3e82665baf51050cf65020b83da36
prama pandit <panditprama@gmail.com>
2022-04-20 13:25:51 +0545

    Create calculater folder in component and also create calc file and set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Calc from './Component/Calculater/Calc'
 
 const App = () => {
   return (
     <>
-      
+      <Calc />
     </>
   )
 }
---
+++ src/Component/Calculater/Calc.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calc = () => {
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default Calc
