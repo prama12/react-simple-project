83ca0bde744a4f4db178fda058b6f982da823908
prama pandit <panditprama@gmail.com>
2022-04-20 13:51:26 +0545

    Create Demo file and set

---
+++ src/Component/ExportImport/Demo.jsx
@@ -0,0 +1,9 @@
+const BestFilm = "Dream";
+const Name = "Prama"
+function Myname() {
+ let surname = "Pandit"
+ return surname;
+}
+
+export default BestFilm;
+export { Name, Myname }
--- src/Component/ExportImport/Main.jsx
+++ src/Component/ExportImport/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import * as prama from "./Demo"
 
 const Main = () => {
   return (
     <>
-     
+ 
     </>
   )
 }
