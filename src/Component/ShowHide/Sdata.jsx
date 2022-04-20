e9bd5a3462490853bad40f92ecd0d7d16a06b7b6
prama pandit <panditprama@gmail.com>
2022-04-20 16:03:51 +0545

    Create new file which is Sdata

---
+++ src/Component/ShowHide/Sdata.jsx
@@ -0,0 +1,94 @@
+const Sdata = [
+     {
+          id: 1,
+          ques: "What is the command to create a new React project?",
+          ans: "npx create-react-app myReactApp"
+     },
+     {
+          id: 2,
+          ques: "What command is used to start the React local development server?",
+          ans: "npm start"
+     },
+     {
+          id: 3,
+          ques: "What is the default local host port that a React development server uses?",
+          ans: "3000"
+     },
+     {
+          id: 4,
+          ques: "What is the children prop?",
+          ans: "A property that lets you nest components in other components "
+     },
+     {
+          id: 5,
+          ques: "When rendering a list using the JavaScript map() method, what is required for each element rendered?",
+          ans: "key"
+     },
+     {
+          id: 6,
+          ques: "What tool does React use to compile JSX?",
+          ans: "Babel"
+     },
+     {
+          id: 7,
+          ques: "How can you optimze performance for a function component that always renders the same way?",
+          ans: "Wrap it in the React.memo higher-order component."
+     },
+     {
+          id: 8,
+          ques: "What is a common use case for ref?",
+          ans: "To directly access a DOM node "
+     },
+     {
+          id: 9,
+          ques: "What is the correct syntax to import a Component from React?",
+          ans: "import { Component } from 'react' "
+     },
+     {
+          id: 10,
+          ques: "What is the command to create a new React project?",
+          ans: "npx create-react-app myReactApp"
+     },
+     {
+          id: 11,
+          ques: "What command is used to start the React local development server?",
+          ans: "npm start"
+     },
+     {
+          id: 12,
+          ques: "What is the default local host port that a React development server uses?",
+          ans: "3000"
+     },
+     {
+          id: 13,
+          ques: "What is the children prop?",
+          ans: "A property that lets you nest components in other components "
+     },
+     {
+          id: 14,
+          ques: "When rendering a list using the JavaScript map() method, what is required for each element rendered?",
+          ans: "key"
+     },
+     {
+          id: 15,
+          ques: "What tool does React use to compile JSX?",
+          ans: "Babel"
+     },
+     {
+          id: 16,
+          ques: "How can you optimze performance for a function component that always renders the same way?",
+          ans: "Wrap it in the React.memo higher-order component."
+     },
+     {
+          id: 17,
+          ques: "What is a common use case for ref?",
+          ans: "To directly access a DOM node "
+     },
+     {
+          id: 18,
+          ques: "What is the correct syntax to import a Component from React?",
+          ans: "import { Component } from 'react' "
+     }
+]
+
+export default Sdata;
