11c8e3de1f2898f6cee8269e61255567633100b2
prama pandit <panditprama@gmail.com>
2022-04-25 09:47:20 +0545

    Tic Tac Toe game complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Calc from './Component/Calculater/Calc'
 import Main from './Component/ExportImport/Main'
 import Home from './Component/GoogleKeepApp/Home'
 import Showfile from './Component/ShowHide/Showfile'
+import TodoHome from './Component/SimpleTodo/TodoHome'
+import TicTac from './Component/TicTacToe/TicTac'
 
 const App = () => {
   return (
@@ -10,7 +12,10 @@ const App = () => {
       {/* <Calc /> */}
       {/* <Main /> */}
       {/* <Showfile/> */}
-      <Home />
+      {/* <Home /> */}
+      {/* <TodoHome /> */}
+      <TicTac />
+
 
     </>
   )
---
+++ src/Component/TicTacToe/TicTac.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react'
+
+
+const TicTac = () => {
+
+     const [game, setGame] = useState('X');
+     const [xChance, setXChance] = useState(Array(9).fill(''));
+     const [win, setWin] = useState();
+
+     const checkWinner = (items) => {
+          let Winner = {
+               across: [
+                    [0, 1, 2],
+                    [3, 4, 5],
+                    [6, 7, 8]
+               ],
+
+               down: [
+                    [0, 3, 6],
+                    [1, 4, 7],
+                    [2, 5, 8]
+               ],
+
+               diagnol: [
+                    [0, 4, 8],
+                    [2, 4, 6]
+               ]
+          };
+
+          for (let winners in Winner) {
+               Winner[winners].forEach((pattern) => {
+                    if (
+                         items[pattern[0]] === "" ||
+                         items[pattern[1]] === "" ||
+                         items[pattern[2]] === ""
+                    ) { }
+                    else if (
+                         items[pattern[0]] === items[pattern[1]] &&
+                         items[pattern[1]] === items[pattern[2]]
+                    ) {
+                         setWin(items[pattern[0]]);
+                    }
+
+               });
+          }
+     };
+
+     const clickEvent = (num) => {
+          if (xChance[num] !== '') {
+               alert("Already Clicked");
+               return;
+          }
+
+          let items = [...xChance];
+
+          if (game === 'X') {
+               items[num] = 'X';
+               setGame('0');
+          } else {
+               items[num] = '0';
+               setGame('X');
+          }
+
+          checkWinner(items);
+          setXChance(items);
+          //console.log(items);
+     }
+
+     const handleRestart = () => {
+          setWin(null);
+          setXChance(Array(9).fill(''));
+     }
+
+
+     const Cell = ({ num , style }) => {
+          return <td className={`${style} border-white text-center w-20 h-20 text-4xl font-black text-white hover:shadow-md shadow-slate-100 cursor-pointer hover:bg-slate-800 rounded-md overflow-hidden `} onClick={() => clickEvent(num)}>{xChance[num]}</td>;
+     }
+
+     return (
+          <>
+               <div className='w-full bg-gray-800'>
+                    <div className='flex h-screen items-center justify-center'>
+                         {/* Tic tac toe */}
+
+                         <div className='flex justify-center items-center flex-col gap-3'>
+                              <h1 className=' font-bold text-4xl text-yellow-400 mb-2'>Turn : {game}</h1>
+                              <table>
+                                   <tr>
+                                        <Cell num={0} style="border-b-4"/>
+                                        <Cell num={1} style="border-x-4"/>
+                                        <Cell num={2} style="border-b-4"/>
+                                   </tr>
+
+                                   <tr>
+                                        <Cell num={3} style="border-r-4"/>
+                                        <Cell num={4} style="border-4"/>
+                                        <Cell num={5} style="border-l-4"/>
+                                   </tr>
+
+                                   <tr>
+                                        <Cell num={6} style="border-t-4"/>
+                                        <Cell num={7} style="border-x-4"/>
+                                        <Cell num={8} style="border-t-4"/>
+
+                                   </tr>
+                              </table>
+
+                              {win && (
+
+                                   <div className=' mt-3'>
+                                        <h1 className='text-red-600 font-bold text-xl'>{win} <span className='text-white'>is the Winner!</span></h1>
+                                        
+                                   </div>
+
+                              )
+                              }
+                              <div className=' mt-3'>
+                              <button className='border-red-600 text-xs font-bold px-4 py-2 rounded-md overflow-hidden bg-red-600 hover:bg-red-800 text-white ' onClick={() => handleRestart()}>Play Again</button>
+                              </div>
+
+                         </div>
+
+                    </div>
+
+               </div>
+          </>
+     )
+}
+
+export default TicTac
